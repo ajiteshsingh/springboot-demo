6b934b0afe893de5101b6ab7ee72b25a32060060
Ajitesh Singh <ajitesh@invideo.io>
2021-02-28 19:50:45 +0530

    simple crud

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -20,6 +21,37 @@ public class FakePersonDataAccessService implements PersonDao{
     @Override
     public List<Person> selectAllPeople() {
         return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> personMaybe = selectPersonById(id);
+        if (personMaybe.isEmpty()) {
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
+    }
+
+    @Override
+    public int updatePersonByid(UUID id, Person person) {
+        return selectPersonById(id)
+                .map(p -> {
+                   int indexOfPersonToDelete = DB.indexOf(p);
+                   if (indexOfPersonToDelete >= 0) {
+                       DB.set(indexOfPersonToDelete, new Person(id, person.getName()));
+                       return 1;
+                   }
+                   return 0;
+                })
+                .orElse(0);
     }
 
 }
